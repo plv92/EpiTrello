fc1faccce2b26290dc5d9c17b8864fff372ca829
plv92 <pierre-louis.vie@epitech.eu>
2025-11-18 10:12:00 +0100

    Initialize project with package.json and package-lock.json

--- epitrello/app/(marketing)/page.tsx
+++ epitrello/app/(marketing)/page.tsx
@@ -1,11 +1,36 @@
 import Link from "next/link";
+import localFont from "next/font/local";
+import { Poppins } from "next/font/google";
 import { Button } from "@/components/ui/button";
 import { Medal } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+const headingFont = localFont({
+    src: "../../public/fonts/font.woff2"
+});
+
+const TextFont = Poppins({
+    subsets: ["latin"],
+    weight: [
+        "100",
+        "200",
+        "300",
+        "400",
+        "500",
+        "600",
+        "700",
+        "800",
+        "900"
+    ],
+})
 
 const MarketingPage =() => {
     return (
         <div className="flex items-center justify-center flex-col">
-            <div className="flex items-center justify-center flex-col">
+            <div className={cn(
+                "flex items-center justify-center flex-col",
+                headingFont.className,
+            )}>
                 <div className="mb-4 flax item-center border shadow-sm p-4 bg-amber-100
                 text-amber-700 rounded-full uppercase">
                     <Medal className="h-6 w-6 mr-2" />
@@ -19,8 +44,10 @@ const MarketingPage =() => {
                     work forward.
                 </div>
             </div>
-            <div className="text-sm md:text-xl text-neutral-400 mt-4
-            max-w-xs md:max-w-2xl text-center mx-auto">
+            <div className={cn(
+                "text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto",
+                TextFont.className,
+                )}>
                 Collaborate, manage projects, and reach new productivity peaks.
                 From high rises to the home office, the way your team works is unique—accomplish it all with Taskify.
             </div>
